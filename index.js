202d8efa0bf43ebe2a63efacb78ee948730a8cf2
AnikKDev <anikdev2016@gmail.com>
2022-05-21 22:46:41 +0600

    completed server setting without jwt

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
+
+// APIs
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
