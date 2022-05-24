97d9adf5d2c67bfd659d9bc09213063436afac5e
AnikKDev <anikdev2016@gmail.com>
2022-05-25 01:25:55 +0600

    checking

--- index.js
+++ index.js
@@ -111,8 +111,16 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await bookingCollection.findOne(query);
             res.send(result);
-        })
+        });
 
+        // delete specific item by id
+
+        app.delete('/booking/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(filter);
+            res.send(result);
+        });
 
         // get all users
         app.get('/user', verifyJWT, async (req, res) => {
